335245d6ea4caa902163a0d68bdc92922146eafe
Fabian Jacquet <tatou@MacBook-Pro-de-Fabian.local>
2026-08-05 16:25:52 +0200

    new design

---
+++ kiko/db/new_fede.sql
@@ -0,0 +1,14 @@
+alter table pratiquants
+    add fede_licence integer;
+
+alter table pratiquants
+    add fede_naissance DATE;
+
+alter table pratiquants
+    add fede_email TEXT;
+
+alter table pratiquants
+    add fede_adresse TEXT;
+
+alter table pratiquants
+    add fede_licence_date DATE;
